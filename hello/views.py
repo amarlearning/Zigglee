022f7c9f5ab368c23f5866e0f27741b3d358e1e8
Amar Prakash Pandey <amar.om1994@gmail.com>
2016-07-31 02:05:14 +0530

    better :octocat:

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+
 from .models import Greeting
 
 # Create your views here.
 def index(request):
     r = requests.get('http://httpbin.org/status/418')
-    print r.text
     return HttpResponse('<pre>' + r.text + '</pre>')
 
 
@@ -18,4 +18,3 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, 'db.html', {'greetings': greetings})
-
