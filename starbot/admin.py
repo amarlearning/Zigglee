2a06264e48eeb4a2cc1952cd4f05b419550e8d4d
Amar Prakash Pandey <amar.om1994@gmail.com>
2016-08-03 02:00:22 +0530

    working on db :baby:

--- starbot/admin.py
+++ starbot/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import githubBots,repository
 
-# Register your models here.
+admin.site.register(githubBots)
+admin.site.register(repository)
--- starbot/models.py
+++ starbot/models.py
@@ -1,8 +1,19 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
 class githubBots(models.Model):
 	username = models.CharField(max_length=250)
 	spamflag = models.CharField(max_length=10)
+
+	def __str__(self):
+		return self.username
+
+
+class repository(models.Model):
+	name = models.CharField(max_length=1000)
+	stars = models.CharField(max_length=5)
+	datetime = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.name
