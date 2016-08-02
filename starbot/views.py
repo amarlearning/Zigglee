310f449eca38e9d225232a71d0e6a533557a35fa
Amar Prakash Pandey <amar.om1994@gmail.com>
2016-08-03 03:29:45 +0530

    looking into machanize

--- starbot/views.py
+++ starbot/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import re, time
+from mechanize import Browser
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hellox from Python!')
+	browser = Browser()
+	response = browser.open("https://github.com/join?source=header-home")
+	return HttpResponse(response)
