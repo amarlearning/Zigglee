8be7869033546ea18f0a804d4cd7c66ab48f679d
Amar Prakash Pandey <amar.om1994@gmail.com>
2016-08-02 05:56:07 +0530

    core structure

--- iStarbot/settings.py
+++ iStarbot/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
+    'starbot.apps.StarbotConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
