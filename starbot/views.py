1f4527686d55de40e3fbf40bc374dd537045e903
Amar Prakash Pandey <amar.om1994@gmail.com>
2016-08-03 03:39:23 +0530

    looking into machanize

--- starbot/views.py
+++ starbot/views.py
@@ -5,6 +5,14 @@ from mechanize import Browser
 
 # Create your views here.
 def index(request):
+	
+	# Create a browser object and give it some optional settings.
 	browser = Browser()
+	browser.set_all_readonly(False)
+	browser.set_handle_robots(False)
+	browser.set_handle_refresh(False)
+
+	# Open a webpage
 	response = browser.open("https://github.com/join?source=header-home")
+
 	return HttpResponse(response)
