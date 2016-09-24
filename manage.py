bf747b5334db987a78423df0c7759310817fe332
Amar Prakash Pandey <amar.om1994@gmail.com>
2016-09-24 21:58:33 +0530

    all changed

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iStarbot.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Recipejaar.settings")
 
     from django.core.management import execute_from_command_line
 
--- music/apps.py
+++ music/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class StarbotConfig(AppConfig):
-    name = 'starbot'
+class MusicConfig(AppConfig):
+    name = 'music'
